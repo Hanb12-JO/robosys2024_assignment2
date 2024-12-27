1bd686a306884cf4f1ded9e5c0c8edb4fd2a260a
Abelrahman Alhanbali <abdelrahman.alhanbali@gmail.com>
2024-12-28 00:58:18 +0900

    Successfully pass in local environment

--- test/test.bash
+++ test/test.bash
@@ -6,11 +6,11 @@ dir=~
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
-timeout 60 ros2 run robosys_assignment2 wifispeed | tee - /tmp/robosys_assignment2.log
-sleep 2
+timeout 60 timeout 60 ros2 launch robosys_assignment2 wifispeed.launch.py | tee - /tmp/robosys_assignment2.log
 
 
-cat /tmp/robosys_assignment2.log | grep 'Published'
+cat /tmp/robosys_assignment2.log | 
+grep 'Mbps'
 #grep 'wifispeed' #&& 'Getting download speed...'
 #grep 'Publish'
 #grep 'wifispeed' && 'Published'
